67cd50ee88903f3b15ec234f8bc0b88bc755b16c
Sungmin yi <ssuyi@ucdavis.edu>
2016-12-15 20:32:33 -0800

    Add files via upload

--- Ctrl.js
+++ Ctrl.js
@@ -6,10 +6,11 @@ app.controller('oneController', function($scope,$timeout,$http) {
 
      $scope.results = [];
  	 $scope.showIt = false;	
+ 	 $scope.saying = "We found result for ";
+ 	 $scope.ActiveAnimation = false;
 
 	function searchFlicker(){
-	$scope.showIt = true;
-	$scope.showText = true;
+	
 	$http({
 	method:"GET",
 	url:"https://api.flickr.com/services/rest",
@@ -21,14 +22,41 @@ app.controller('oneController', function($scope,$timeout,$http) {
 		    nojsoncallback: 1
 		}
 	}).success(function(data){
+		$scope.showIt = true;
+		$scope.showText = true;
 		$scope.results = data;
 		$timeout(function(){
 			$scope.showText = false;
 
 		},1500);
 	}).error(function(error){
+		$scope.saying = "Our server crashed";
 		console.error(error);
 	});
 
 };
+///////////
+// vm = this;
+// vm.getAllTasks = function(){
+// 	var defer = $q.defer();
+// 	$http.get()
+// 	.success(function(res){
+// 		defer.resolve(res);
+// 	})
+// 	.error(function(err, status){
+// 		defer.reject(err);
+// 	})
+// return defer.promise;
+// }
+
+// vm.createTask = function(task){
+// 	var defer = $q.defer();
+// 	$http.post()
+
+// 	return defer.promise;
+// }
+
+
+
+/////////
 });
